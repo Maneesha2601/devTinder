7670589af7bd535feec101c9570b0083f394dcc5
Maneesha2601 <maneeshareddykonda2601@gmail.com>
2025-04-04 15:31:35 +0530

    added_dotenvlib

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
 
 const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://maneeshareddykonda2601:EGlghSl4whrG9MS4@devtinder.g343hqv.mongodb.net/");
+    await mongoose.connect(process.env.URI);
 }
 
 module.exports=connectDB;
